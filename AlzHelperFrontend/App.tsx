3636755bc74f09f850f14d27ab7cfbd33c02660a
root <root@Elyes.>
2025-07-23 22:09:12 +0100

    modifications

--- AlzHelperFrontend/App.tsx
+++ AlzHelperFrontend/App.tsx
@@ -3,10 +3,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import CaregiverDashboard from './screens/CaregiverDashboard';
+import DoctorDashboard from './screens/DoctorDashboard';
+import AddReportScreen from './screens/AddReportScreen';
+import ViewReportsScreen from './screens/ViewReportsScreen';
+import AddNoteScreen from './screens/AddNoteScreen';
 
 export type RootStackParamList = {
   Login: undefined;
   Register: undefined;
+  CaregiverDashboard: undefined;
+  DoctorDashboard: undefined;
+  AddReportScreen: undefined;
+  ViewReportsScreen: undefined;
+  AddNoteScreen: { report: any };
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -17,6 +27,11 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="CaregiverDashboard" component={CaregiverDashboard} options={{ headerShown: false }} />
+        <Stack.Screen name="DoctorDashboard" component={DoctorDashboard} options={{ headerShown: false }} />
+        <Stack.Screen name="AddReportScreen" component={AddReportScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ViewReportsScreen" component={ViewReportsScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="AddNoteScreen" component={AddNoteScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
