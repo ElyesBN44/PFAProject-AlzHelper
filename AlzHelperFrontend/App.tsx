040386634421496c629f3c090632227a8b492d26
root <root@Elyes.>
2025-07-24 12:12:33 +0100

    fixed render error

--- AlzHelperFrontend/App.tsx
+++ AlzHelperFrontend/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider as PaperProvider } from 'react-native-paper';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import CaregiverDashboard from './screens/CaregiverDashboard';
@@ -27,18 +28,20 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="CaregiverDashboard" component={CaregiverDashboard} options={{ headerShown: false }} />
-        <Stack.Screen name="DoctorDashboard" component={DoctorDashboard} options={{ headerShown: false }} />
-        <Stack.Screen name="AddReportScreen" component={AddReportScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="ViewReportsScreen" component={ViewReportsScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="AddNoteScreen" component={AddNoteScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="PatientDetailsScreen" component={PatientDetailsScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="AddPatientScreen" component={AddPatientScreen} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="CaregiverDashboard" component={CaregiverDashboard} options={{ headerShown: false }} />
+          <Stack.Screen name="DoctorDashboard" component={DoctorDashboard} options={{ headerShown: false }} />
+          <Stack.Screen name="AddReportScreen" component={AddReportScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="ViewReportsScreen" component={ViewReportsScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="AddNoteScreen" component={AddNoteScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="PatientDetailsScreen" component={PatientDetailsScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="AddPatientScreen" component={AddPatientScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
