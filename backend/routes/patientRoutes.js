4fca95cfb95ff0ec6aed6b4607a067f6422d1d5d
root <root@Elyes.>
2025-07-24 11:00:58 +0100

    calling report controller

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patientController');
+const reportController = require('../controllers/reportController');
 
 // Create a new patient
 router.post('/', patientController.createPatient);
@@ -10,5 +11,8 @@ router.get('/', patientController.getAllPatients);
 
 // Get a single patient by ID
 router.get('/:id', patientController.getPatientById);
+
+// Get all reports for a specific patient
+router.get('/:id/reports', reportController.getReportsByPatient);
 
 module.exports = router; 
